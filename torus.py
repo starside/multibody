2313d5cad000d448b41e8434dc428334cd15e2c3
Josh Kelly <inst.zombie@gmail.com>
2017-06-20 21:42:24 -0700

    Torus works

--- torus.py
+++ torus.py
@@ -9,6 +9,7 @@ def buildTorus(width):
     g = Graph(directed=False)
     for i in range(width*width):    # Add vertices
         g.add_vertex()
+    #Build grid mesh
     for row in range(width):
         for col in range(width):
             m = row*width + col
@@ -16,8 +17,13 @@ def buildTorus(width):
                 g.add_edge(m, m+1)
             if row != width - 1:
                 g.add_edge(m, m + width)
-    
+    #Connect edges in period boundary condition
+    for row in range(width):
+        g.add_edge(row*width, row*width + width - 1)
+    for col in range(width):
+        g.add_edge(col, width*width - width + col)
     return g
 
-t = buildTorus(4)
+t = buildTorus(50)
 print t
+graph_draw(t, vertex_font_size=20, output_size=(600, 600) )
